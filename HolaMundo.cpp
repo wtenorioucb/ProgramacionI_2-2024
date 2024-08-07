6839c6206084721d64b011374e0cc6c27a9f0cc9
Willy Tenorio <wtenorio@ucb.edu.bo>
2024-08-07 19:08:08 -0400

    Comentarios

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,3 +1,9 @@
+/*
+    Creador: Willy Tenorio
+    Fecha creación: 07-08-2024
+    Descripción: Mostrar el funcionamiento de un programa en C++
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -5,10 +11,11 @@ using namespace std;
 int main()
 {
     int numero;
-    cout << "MI PRIMER PROGRAMA" << endl;
+    system("cls"); // Limpiar Pantalla
+    cout << "MI PRIMER PROGRAMA\n";
     cout << "==================" << endl;
-    cout << "Ingrese un numero: ";
+    cout << "\tIngrese un numero: ";
     cin >> numero;
-    cout << "El numero ingresado es: " << numero;
+    cout << "\tEl numero ingresado es: " << numero;
     return 0;
 }

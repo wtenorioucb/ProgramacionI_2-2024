9ecbefe3f351e5c34a6c6cbb40c3db1394e94a16
Willy Tenorio <wtenorio@ucb.edu.bo>
2024-08-14 16:53:12 -0400

    Programa Hola Mundo

---
+++ HolaMundo2.cpp
@@ -0,0 +1,15 @@
+/* Creador: Willy Tenorio
+   Fecha creación: 14-08-2024
+   Descripción: Programa de hola mundo */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    cout << "Hola mundo.\n";
+    system("pause");
+    return 0;
+}
